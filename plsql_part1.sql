81a2bc5109728e9b9d79a8be2c08495ae04e9038
Yamika Perera <40973981+Maduz0097@users.noreply.github.com>
2022-04-06 10:12:25 +0530

    Update plsql_part1.sql

--- plsql_part1.sql
+++ plsql_part1.sql
@@ -615,6 +615,175 @@ END;
 /
 
   
-  
-  
+DECLARE
+	CURSOR cur_emp (p_dept_id INT) IS
+		SELECT employee_id, first_name
+		FROM employees
+		WHERE department_id = p_dept_id;
+	
+	vn_emp_id EMPLOYEES.employee_id%TYPE;
+	vn_emp_first_name EMPLOYEES.first_name%TYPE;
+BEGIN
+	OPEN cur_emp(100);
+	
+	LOOP
+		FETCH cur_emp
+		INTO vn_emp_id, vn_emp_first_name;
+		
+		EXIT WHEN cur_emp%NOTFOUND;
+		
+		dbms_output.put_line(vn_emp_id || ' ' || vn_emp_first_name);
+	END LOOP;
+	
+	CLOSE cur_emp;
+END;
+/
 
+
+
+DECLARE
+	CURSOR cur_emp (p_dept_id INT) IS
+		SELECT employee_id, first_name
+		FROM employees
+		WHERE department_id = p_dept_id;
+BEGIN
+	FOR vn_emp IN cur_emp(100)
+	LOOP
+		dbms_output.put_line(vn_emp.employee_id || ' ' || vn_emp.first_name);
+	END LOOP;
+END;
+/
+
+
+
+DECLARE
+	CURSOR cur_emp IS
+		SELECT UNIQUE department_id
+		FROM employees
+		WHERE ROUND((SYSDATE - HIRE_DATE) / 365) >= 25;
+	CURSOR cur_dept (p_dept_id INT) IS
+		SELECT department_name
+		FROM departments
+		WHERE department_id = p_dept_id;
+		
+	vn_dept_name DEPARTMENTS.DEPARTMENT_NAME%TYPE;
+BEGIN
+	FOR vn_emp IN cur_emp
+	LOOP
+		OPEN cur_dept(vn_emp.department_id);
+		
+		FETCH cur_dept
+		INTO vn_dept_name;
+		
+		dbms_output.put_line(vn_dept_name);
+		
+		CLOSE cur_dept;
+	END LOOP;
+END;
+/
+
+
+
+DECLARE
+	CURSOR cur_emp IS
+		SELECT department_id, employee_id
+		FROM employees
+		WHERE ROUND((SYSDATE - HIRE_DATE) / 365) >= 25;
+	CURSOR cur_dept (p_dept_id INT) IS
+		SELECT department_name
+		FROM departments
+		WHERE department_id = p_dept_id;
+		
+	vn_dept_name DEPARTMENTS.DEPARTMENT_NAME%TYPE;
+BEGIN
+	FOR vn_emp IN cur_emp
+	LOOP
+		OPEN cur_dept(vn_emp.department_id);
+		
+		FETCH cur_dept
+		INTO vn_dept_name;
+		
+		dbms_output.put_line(vn_dept_name || ' ' || vn_emp.employee_id);
+		
+		CLOSE cur_dept;
+	END LOOP;
+END;
+/
+
+
+
+DECLARE
+	vc_fname EMPLOYEES.FIRST_NAME%TYPE;
+BEGIN
+	BEGIN
+		SELECT first_name
+		INTO vc_fname
+		FROM EMPLOYEES
+		WHERE employee_id = 1500;
+		
+		dbms_output.put_line(vc_fname);
+		
+		EXCEPTION
+			WHEN NO_DATA_FOUND THEN
+				dbms_output.put_line('Employee does not exist');
+			WHEN OTHERS THEN
+				dbms_output.put_line('In Others');
+	END;
+
+	BEGIN
+		SELECT first_name
+		INTO vc_fname
+		FROM employees
+		WHERE department_id = 80;
+		
+		EXCEPTION
+			WHEN TOO_MANY_ROWS THEN
+				dbms_output.put_line('Too many rows to a single value');
+			WHEN OTHERS THEN
+				dbms_output.put_line('In Others');
+	END;
+END;
+/  
+  
+DECLARE
+ e_emp_remaining EXCEPTION;
+ 
+ PRAGMA EXCEPTION_INIT(e_emp_remaining,-02292);
+ 
+ BEGIN
+ 
+ DELETE FROM departments
+ WHERE department_id = &enterdept;
+ COMMIT;
+ EXCEPTION
+ WHEN e_emp_remaining THEN
+  DBMS_OUTPUT.PUT_LINE('Cannot remove department');
+  DBMS_OUTPUT.PUT_LINE('displaying sql code relevent to EXCEPTION :' || SQLCODE);
+  DBMS_OUTPUT.PUT_LINE('displaying sql message relevent to EXCEPTION : ' || SQLERRM);
+  DBMS_OUTPUT.PUT_LINE('displaying sql user relevent to :' || USER);
+  DBMS_OUTPUT.PUT_LINE('displaying sql date relevent to EXCEPTION:'|| SYSDATE);
+  
+  END;
+  /
+  
+  DECLARE
+  invalid_salary EXCEPTION;
+ CURSOR cur_emp IS 
+ SELECT employee_id,first_name,salary,(salary + 1500) AS new_salary FROM employees;
+
+BEGIN 
+ FOR rec IN cur_emp LOOP
+ BEGIN
+    IF rec.new_salary > 4000
+	THEN
+	RAISE invalid_salary;
+	ELSE
+	DBMS_OUTPUT.PUT_LINE(rec.new_salary);
+	END IF;
+	 EXCEPTION WHEN invalid_salary THEN
+  DBMS_OUTPUT.PUT_LINE('invalid_salary');
+  END;
+  END LOOP;
+ 
+ END;
+/
